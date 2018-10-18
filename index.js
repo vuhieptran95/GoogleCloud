6eb2114a453a244b0712fcb1770c405a84963586
Hiep <vuhiep.tran95@gmail.com>
2018-10-18 15:26:01 +0700

    Add post submit

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("App start");
+});
+
+app.get("/submit", (req, res) => {
+  res.sendFile(path.join(__dirname, "/view/form.html"));
+});
+
+app.post("/submit", (req, res) => {
+  console.log({
+    name: req.body.name,
+    message: req.body.message
+  });
+  res.send("Thanks for messaging");
 });
 
 app.listen(process.env.PORT || 1234, () => {
